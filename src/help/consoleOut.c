0c75345bf0658f1ba2285d7682bb2065c6c8a974
Matt <mdbaker60@gmail.com>
2013-04-10 03:37:11 -0400

    Char out of for

--- src/help/consoleOut.c
+++ src/help/consoleOut.c
@@ -13,8 +13,9 @@ void printHelp(){
 //Read flags and dispatch proper help message
 void parseFlg(int argc, char** argv){
 	int i = 1;
-	char flag = *(*(argv + i)+1);
+	char flag; = *(*(argv + i)+1);
 	for (;i < argc; i++){
+		flag = *(*(argv + i)+1);
 		switch(flag){
 			case('h'):
 				printf("HELP FLAG");	
