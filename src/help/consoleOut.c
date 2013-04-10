f84d3f22f13ca8d1305d299537d2850db96eb078
Matt <mdbaker60@gmail.com>
2013-04-09 22:16:48 -0400

    Added stub help code

---
+++ src/help/consoleOut.c
@@ -0,0 +1,14 @@
+//Console help library
+//Add help stuff here
+#include "help.h"
+
+//Generic help message
+void printHelp(){
+	printf("You seem lost, friend.\n");
+}
+
+
+//Read flags and dispatch proper help message
+void parseFlg(char* argv[]){
+	printf("Reading Flags!");
+}
---
+++ src/help/help.h
@@ -0,0 +1,6 @@
+#ifndef help_h
+#define help_h
+#include "../main.h"
+	void printHelp();
+	void parseFlg(char**);
+#endif
